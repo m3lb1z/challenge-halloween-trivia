e06cdec4b318a6731d23fe2fb645e5a821534341
Elvis M. <u201112959@upc.edu.pe>
2024-10-29 12:17:23 -0500

    feat: Add Maven wrapper and project structure

---
+++ src/main/java/emrx/halloween/ChallengeHalloweenTriviaApplication.java
@@ -0,0 +1,13 @@
+package emrx.halloween;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeHalloweenTriviaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeHalloweenTriviaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/emrx/halloween/ChallengeHalloweenTriviaApplicationTests.java
@@ -0,0 +1,13 @@
+package emrx.halloween;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeHalloweenTriviaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
