04f21aeed2edf34fc2f00492417ac4f44d1135e5
Elvis M. <u201112959@upc.edu.pe>
2024-10-30 17:24:41 -0500

    feat: Add questions and categories to the database.

---
+++ src/main/resources/insert.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO public.category (id, name) VALUES (1, 'Vampiros');
+INSERT INTO public.category (id, name) VALUES (2, 'Zombis');
+INSERT INTO public.category (id, name) VALUES (3, 'Fantasmas');
+INSERT INTO public.category (id, name) VALUES (4, 'Esqueletos');
+INSERT INTO public.category (id, name) VALUES (5, 'Momias');
+INSERT INTO public.category (id, name) VALUES (6, 'Hombres lobo');
+INSERT INTO public.category (id, name) VALUES (7, 'Brujas');
+INSERT INTO public.category (id, name) VALUES (8, 'Hechiceros');
+INSERT INTO public.category (id, name) VALUES (9, 'Nigromantes');
+INSERT INTO public.category (id, name) VALUES (10, 'Demonios');
+INSERT INTO public.category (id, name) VALUES (11, 'Poltergeists');
+INSERT INTO public.category (id, name) VALUES (12, 'Trolls');
+INSERT INTO public.category (id, name) VALUES (13, 'Golem');
+INSERT INTO public.category (id, name) VALUES (14, 'Mothman');
